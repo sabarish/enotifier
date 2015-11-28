a957b25d353f328aee429282585438afc7317780
Sabarish Sankar <sabarish@chronus.com>
2015-11-29 00:18:52 +0530

    First commit - base app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Eventsmailer::Application.config.secret_token = '0c027c7c8fcfeeefb28a151e4c3cfe076bd1f51356b2c4f86055dff6b7f3c9572190faeaa802b570d446e6a7be9b52d0a51e56cb59f253c7103367340e943c37'
