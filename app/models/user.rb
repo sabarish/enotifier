a1f0bfc480e9e58cfa49d161a011f3d6f8fddb3c
Sabarish Sankar <sabarish@chronus.com>
2015-11-29 13:59:17 +0530

    Adding name to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ActiveRecord::Base
   validates :first_name, :last_name, :email, presence: true
   validates :email, uniqueness: true
 
+  def name
+    "#{self.first_name} #{self.last_name}"
+  end
 end
