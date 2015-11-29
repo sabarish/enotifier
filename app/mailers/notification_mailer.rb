ed37a23adca9194063fd5957a1b6fe2d6121cf6a
Sabarish Sankar <sabarish@chronus.com>
2015-11-29 13:56:53 +0530

    Updating mailer content

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -4,6 +4,14 @@ class NotificationMailer < ActionMailer::Base
  
   def daily_notification(group)
     @group = group
-    mail(to: @group.email, subject: "Welcome!")
+    @notifications = @group.fetch_users_with_notifications_for_date
+    @birthdays = @notifications[0]
+    @weddings = @notifications[1]
+    @office_anniversaries = @notifications[2]
+    subject = []
+    subject << "Happy Birthday" if @birthdays.present?
+    subject << "Happy Wedding Day" if @weddings.present?
+    subject << "Happy Office Anniversary" if @office_anniversaries.present?
+    mail(to: @group.email, subject: subject.to_sentence)
   end
 end
