29addc32864a010760a52ca7e2c6e7477e809a6f
Sabarish Sankar <sabarish@chronus.com>
2017-01-08 19:16:57 +0530

    Adding devise to the application

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20151129072635) do
+ActiveRecord::Schema.define(:version => 20170108063922) do
 
   create_table "groups", :force => true do |t|
     t.string   "name",        :null => false
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(:version => 20151129072635) do
     t.integer  "user_id",     :null => false
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
+    t.string   "email"
   end
 
   create_table "user_groups", :force => true do |t|
@@ -29,19 +30,40 @@ ActiveRecord::Schema.define(:version => 20151129072635) do
   end
 
   create_table "users", :force => true do |t|
-    t.string   "first_name",           :null => false
+    t.string   "first_name",                             :null => false
     t.string   "last_name"
     t.string   "email"
     t.date     "birth_date"
     t.date     "wedding_date"
     t.boolean  "is_admin"
-    t.datetime "created_at",           :null => false
-    t.datetime "updated_at",           :null => false
+    t.datetime "created_at",                             :null => false
+    t.datetime "updated_at",                             :null => false
     t.string   "current_organization"
     t.string   "current_position"
     t.date     "joining_date"
     t.text     "current_location"
     t.string   "contact_number"
+    t.string   "encrypted_password",     :default => "", :null => false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          :default => 0,  :null => false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string   "unconfirmed_email"
+    t.integer  "failed_attempts",        :default => 0,  :null => false
+    t.string   "unlock_token"
+    t.datetime "locked_at"
   end
+
+  add_index "users", ["confirmation_token"], :name => "index_users_on_confirmation_token", :unique => true
+  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
+  add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
+  add_index "users", ["unlock_token"], :name => "index_users_on_unlock_token", :unique => true
 
 end
